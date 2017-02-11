795de8aaca41b4c87b3b297e1ab0e08da036a246
Martin Duffy <duffym4@rpi.edu>
2017-02-11 17:18:01 -0500

    Some player/collisions stuff

--- src/player.lua
+++ src/player.lua
@@ -6,20 +6,23 @@ Player = class()
 function Player:_init(game)
 	self.game = game
 
-	self.x = 0		--Players X and Y location
-	self.y = 0
+	self.x = 300		--Players X and Y location
+	self.y = 300
 
 	self.dx = 0		--Players X and Y velocity
 	self.dy = 0
-	self.ddx = 5	--Players X acceleration
+	self.speed = 160*2	--Players X acceleration
 
 	self.onGround = false		--Tracks if the player is on the ground
 	self.onLadder = false		--Tracks if the player is on a ladder
+	
+	self.w = 160
+	self.h = 2*160
 
-	self.gravity = -30			--Attributes about they players movement
-	self.jumpStrength = 30		--and world
+	self.gravity = 16			--Attributes about they players movement
+	self.jumpStrength = 160*4		--and world
 	self.ladderSpeed = 5 		--Rate at which a player climbs ladders
-	self.maxSpeed = 7
+	self.maxSpeed = 70
 	self.friction = 2
 end
 
@@ -44,58 +47,77 @@ end
 --Lets the player jump
 function Player:jump()
 	if self.onGround then
-		self.dy = self.jumpStrength
+		self.dy = -self.jumpStrength
+		self.y = self.y - 100
+		error("OKAY")
 	end
 end
 
 --Draws the rectangle
 function Player:draw()
-	love.graphics.rectangle("fill", self.x, self.y, 50, 50)		--Placeholder
+	love.graphics.rectangle("fill", self.x, self.y, self.w, self.h)		--Placeholder
 end
 
 function Player:keypressed(key)
-	if key == "space" then
-		self:jump()
-	end
+
 end
 
 --Runs collision checking
-function Player:collisions(level)
+function Player:collisions(dt, level)
 	--level has level.walls, which holds (x, y, w)
 	for i, wall in pairs(level.walls) do
-		if self.dx < 0 and self.x < wall.x then
-			self.dx = 0
-			self.x = wall.x
+		if self.x + self.w > wall.x and self.x < wall.x + wall.w then
+			if self.y + self.h <= wall.y + 1 and self.y + self.h + self.dy*dt > wall.y then
+				self.dy = 0
+				self.y = wall.y - self.h
+				self.onGround = true
+			end
+		end
+		if self.y + self.h > wall.y and self.y < wall.y + wall.w then
+			--if self.x + self.w <= wall.x + 1 and self.x + self.w + self.dx*dt > wall.x then
+			--	self.dx = 0
+			--	self.x = wall.x - self.w
+			if self.x >= wall.x + wall.w and self.x + self.dx*dt < wall.x + wall.w then
+				self.dx = 0
+				self.x = wall.x + wall.w
+			end
 		end
 	end
 end
 
+function Player:getInput()
+	if love.keyboard.isDown("a") then		--accelerates the player left
+		self.dx =  -self.speed
+	end
+	if love.keyboard.isDown("d") then	--accelerates the player right
+		self.dx = self.speed
+	end
+	if love.keyboard.isDown("w") and self.onLadder then
+		self.y = self.ladderSpeed
+	end
+	if love.keyboard.isDown("d") and self.onLadder then
+		self.y = -1 * self.ladderSpeed
+	end
+	if love.keyboard.isDown("space") and self.onGround then
+		self.dy = -self.jumpStrength
+	end
+end
 
 --Moves the player
-function Player:update(dt)
-
-	if love.keyboard.isDown("a") then		--accelerates the player left
-		self.dx = self.dx - self.ddx
-	elseif love.keyboard.isDown("d") then	--accelerates the player right
-		self.dx = self.dx + self.ddx
-	elseif love.keyboard.isDown("w") and self.onLadder then
-		self.y = self.ladderSpeed
-	elseif love.keyboard.isDown("d") and self.onLadder then
-		self.y = -1 * self.ladderSpeed
-	--else
-	--	if math.abs(self.dx) > 0.1 and self.onGround then		--Slows the player down based on the friction coefficient.  THIS MIGHT HAVE A BUG WITH LADDERS!!!
-		--	self.dx = self.dx - sign(self.dx) * self.friction
-	--	end
-	end
+function Player:update(dt, level)
+	
+	self.dx = 0
+	self.onGround = false
+	
+	self:collisions(dt, level)
+	self:getInput()
 
 	if not self.onLadder then
-		self.dy = self.dy + self.gravity * dt
+		self.dy = self.dy + self.gravity
 		self.y = self.y + self.dy * dt
 	end
 	
-	if math.abs(self.dx) > self.maxSpeed then	--makes sure the player isn't moving too fast
-		self.dx = sign(self.dx) * self.maxSpeed
-	end
 	self.x = self.x + self.dx * dt	--moves the player
+	
 end
 
