b3158f5976f68c93081cbe7d3b75b9f93f5fe9f0
Martin Duffy <duffym4@rpi.edu>
2017-02-12 05:33:28 -0500

    Fixes to press e

--- src/player.lua
+++ src/player.lua
@@ -79,6 +79,12 @@ end
 
 --Draws the rectangle
 function Player:draw(level, camera)
+
+	if self:isTouchingInteractable(level)[1]~="nothing" then
+		love.graphics.setFont(love.graphics.newFont("fonts/november.ttf", 36))
+		love.graphics.printf("PRESS E", self:isTouchingInteractable(level)[3] + camera.x, self:isTouchingInteractable(level)[4] + camera.y - 80, level.tileSize, "center")
+	end
+
 	if self.animationType == "still" then
 		if self.facing==1 then
 			love.graphics.draw(self.idleImages[math.floor(self.animation)+1], self.x + camera.x - self.w/2, self.y + camera.y, 0, self.facing, 1)		--Placeholder
@@ -98,11 +104,6 @@ function Player:draw(level, camera)
 		else
 			love.graphics.draw(self.climbImages[math.floor(self.animation)+1], self.x + camera.x + 3*self.w/2, self.y + camera.y, 0, self.facing, 1)		--Placeholder
 		end
-	end
-	
-	if self:isTouchingInteractable(level)[1]~="nothing" then
-		love.graphics.setFont(love.graphics.newFont("fonts/joystixMonospace.ttf", 24))
-		love.graphics.printf("PRESS E", self:isTouchingInteractable(level)[3] + camera.x, self:isTouchingInteractable(level)[4] + camera.y - 80, level.tileSize, "center")
 	end
 	--love.graphics.rectangle("line", self.x + camera.x, self.y + camera.y, self.w, self.h)		--Placeholder
 	
